439042fedc080e1d7ee3dbb82cb47a41a63eac88
Newtype6 <lwhite4@sky.com>
2023-03-28 20:35:34 +0000

    still trying to fix messages

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,9 +1,9 @@
 -- this file was manually created
 INSERT INTO public.users (display_name, handle, email, cognito_user_id)
 VALUES
-  ('Andrew Brown', 'andrewbrown' , 'test@test', 'MOCK'),
+  ('Andrew Brown', 'andrewbrown' , 'test@test', 'ea990873-8101-4980-8ba6-50d7407d3e8e'),
   ('John King', 'JK', 'newtype.jk@googlemail.com', 'ea990873-8101-4980-8ba6-50d7407d3e8e'),
-  ('Andrew Bayko', 'bayko' , 'anothertest@test', 'MOCK');
+  ('Andrew Bayko', 'bayko' , 'anothertest@test', 'ea990873-8101-4980-8ba6-50d7407d3e8e');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
