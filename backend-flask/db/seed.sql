8c716d46927030a963edd52a7d6b8cd4400b3aff
Newtype6 <lwhite4@sky.com>
2023-03-28 21:13:42 +0000

    trouble with users

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -3,7 +3,7 @@ INSERT INTO public.users (display_name, handle, email, cognito_user_id)
 VALUES
   ('Andrew Brown', 'andrewbrown' , 'test@test', 'ea990873-8101-4980-8ba6-50d7407d3e8e'),
   ('John King', 'JK', 'newtype.jk@googlemail.com', 'ea990873-8101-4980-8ba6-50d7407d3e8e'),
-  ('Andrew Bayko', 'bayko' , 'anothertest@test', 'ea990873-8101-4980-8ba6-50d7407d3e8e');
+  ('Andrew Bayko', 'bayko' , 'lwhite4@sky.com', 'cee56b8f-9b27-43e8-9670-4355e4245f58');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
